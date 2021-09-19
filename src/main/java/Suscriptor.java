88aeee0f89a596a7c41383c8de0b8a1ab6b1704a
Crist-C <castro.cristiank@gmail.com>
2021-09-18 23:24:54 -0500

    Se desarrolla la totalidad del reto, el cual no cuenta con la validacion de datos ya que no se solicita. Para la creación de la lista de objetos de usa el ArrayList y se crea un objeto tipo lista. No se usa por medio de propiamente Arrays, se creará otra versión para este caso.

--- src/main/java/Suscriptor.java
+++ src/main/java/Suscriptor.java
@@ -22,22 +22,64 @@ public class Suscriptor {
     public Suscriptor() {
     }
 
-    public Suscriptor(int codigo, String estado, int periodicidad) {
+    public Suscriptor(long codigo, char estado, int periodicidad) {
         this.codigo = codigo;
         this.estado = estado;
         this.periodicidad = periodicidad;
     }
 
 
-    private int codigo;
-    private String estado;
+
+
+    private long codigo;
+    private char estado;
     private int periodicidad;
     
     
-    public int CalcularPago() {
+    public long CalcularPago() {
+        long pago = 0;
+        if(estado == 'V' || estado == 'v'){
+            
+            switch(periodicidad){
+                case 1: pago =  30000;
+                    break;
+                case 2: pago =  70000;
+                    break;
+                case 3: pago =  150000;
+                    break;
+                case 4: pago =  270000;
+                    break;
+            }
+            
+        } else if(estado == 'S' || estado == 's') pago = 0;
         
+        return pago;
         
-        return 1;
+    }
+    
+    
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public char getEstado() {
+        return estado;
+    }
+
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+
+    public int getPeriodicidad() {
+        return periodicidad;
+    }
+
+    public void setPeriodicidad(int periodicidad) {
+        this.periodicidad = periodicidad;
     }
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -8,14 +8,73 @@
  *
  * @author Crist
 */
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class main {
     
     public static void main (String [] args){
         
+        Scanner consola = new Scanner(System.in);
+        
+        int N_Suscriptores;
+        long codigo;
+        char estado;
+        int periodicidad;
+        long pagoTotal = 0;
+        //Suscriptor suscriptores[];
+        //suscriptores = new Suscriptor[2];
+        Suscriptor suscriptor;
+        List<Suscriptor> suscriptores = new ArrayList<Suscriptor>();
+        
+        System.out.println("------------------------------------");
+        System.out.println("----  MINTIC - RETO DE REFUERZO ----");
+        System.out.println("----                            ----");
+        System.out.println("----  Liquidación servicio de   ----");
+        System.out.println("----  suscripción a periódico   ----");
+        System.out.println("----                            ----");
+        System.out.println("------------------------------------");
+        
+        System.out.println("\nIngrese la cantidad de suscriptores: ");
+        N_Suscriptores = consola.nextInt();
         
         
+        for(int i = 0; i < N_Suscriptores; i++){
+            
+            System.out.println("----------------------------------------");
+            System.out.println("||   INFORMACIÓN DEL                   ||");
+            System.out.println("||            SUSCRIPTOR "+(i+1)+"         ||");
+            System.out.println("----------------------------------------");
+            
+            System.out.println("Ingrese el número del código : ");
+            codigo = consola.nextInt();
+            System.out.println("Ingrese el estado del cliente (V=Vigente, S=Suspendido) : ");
+            estado = consola.next().charAt(0);
+            
+            System.out.println("Ingrese el número que corresponda a la periodicidad: ");
+            System.out.println("_____________________________________________");
+            System.out.println("|   Periodicidad     |     Valor a Pagar     |");
+            System.out.println("|____________________________________________|");
+            System.out.println("|        1           |        $30.000        |");
+            System.out.println("|        2           |        $70.000        |");
+            System.out.println("|        3           |       $150.000        |");
+            System.out.println("|        4           |       $270.000        |");
+            System.out.println("|____________________________________________|\n");
+            periodicidad = consola.nextInt();       
+            
+            suscriptor = new Suscriptor(codigo, estado, periodicidad);
+            suscriptores.add(i, suscriptor);
+            
+            pagoTotal += suscriptores.get(i).CalcularPago();
+            
+        }
+       
+        for(int i = 0; i < suscriptores.size(); i++){
+            System.out.println("El valor a pagar para el suscriptor del código "
+                +suscriptores.get(i).getCodigo()+" es: "+suscriptores.get(i).CalcularPago());
+        }
+            System.out.println("El valot total por pagar es: "+pagoTotal);
         
     }
     
